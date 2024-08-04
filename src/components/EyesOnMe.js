a2a48bf728614276f082d37767cd55f550f8c7b3
kibiruki <+>
2024-08-05 01:32:53 +0300

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    //evenlistener for focus
+    const handleFocus = () => {
+        console.log('Good!');
+    };
+
+    //event listerner for blur
+    const handleBlur = () => {
+        console.log('Hey! Eyes on me!');
+    };
+
+    return (
+        <div>
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    //event listener
+    const handleCHange = () => {
+        console.log("Entering password...");
+    };
     return (
-        <div></div>
+        <div>
+            <input 
+            type="password"
+             onChange={handleCHange}
+            placeholder="Enter your password"
+            />
+        </div>
     )
+
 }
+
 
 export default Keypad;
